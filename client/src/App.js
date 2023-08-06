921d3735df97af928e8d589808adeb6b94cc6370
tinar9678 <tina.r9678@gmail.com>
2023-08-06 14:49:06 -0400

    Added static frontend page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import image from './images/crystal-ball.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <div className="text-sentiment-app-header">
+        <img className="crystal-ball-img" src={image} alt="Crystal Ball Emoji"/>
+        <h1>text sentiment reader</h1>
+      </div>
+      <div className="text-sentiment-input-area">
+        <span className="input-label">input text here: </span>
+        <textarea className="input-text-box" placeholder="write a text and i'll tell you its underlying sentiment." rows={6}></textarea>
+      </div>
+      <div className="text-sentiment-output-area">
+        <span className="output-label">Sentiment is </span>
+        <h3 className="output-text">placeholder</h3>
+      </div>
     </div>
   );
 }
